db4c63987e5cbd79f05ea01ea46bfd405102119e
Bhav Sagar <bhavsagar5@gmail.com>
2023-08-02 16:04:51 +0530

    - added no-cache

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,7 +5,7 @@ export const GET = async (req, res) => {
   try {
     await connectToDB();
     const prompts = await Prompt.find({}).populate("creator");
-
+    //revalidatePath(req.url);
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
